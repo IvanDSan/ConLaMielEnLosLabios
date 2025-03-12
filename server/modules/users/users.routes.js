f39744dd7dbba2d550c4be1e5f66c932dd10e50a
Sanan <senan996@gmail.com>
2025-03-13 00:18:35 +0100

    feat/login, register, recoveryPassword, verify

--- server/modules/users/users.routes.js
+++ server/modules/users/users.routes.js
@@ -4,4 +4,9 @@ import usersController from './users.controllers.js';
 // Endpoint: http://localhost:4000/users
 const router = express.Router();
 
+router.post('/register', usersController.register);
+router.post('/login', usersController.login);
+router.get('/verify/:token', usersController.verify);
+router.get('/recoveryPassword', usersController.recoveryPassword);
+
 export default router;
