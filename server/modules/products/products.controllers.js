9dbd8f2f021e65d06ffebb076fffd6ed6b790cab
Sanan <senan996@gmail.com>
2025-03-19 11:17:10 +0100

    fix/delete product controller

--- server/modules/products/products.controllers.js
+++ server/modules/products/products.controllers.js
@@ -1,4 +1,4 @@
-import executeQuery from "../../config/db.js";
+import executeQuery from '../../config/db.js';
 
 class ProductsController {
   createProduct = async (req, res) => {
@@ -8,17 +8,20 @@ class ProductsController {
       if (!title || !description || !price || !category_id) {
         return res
           .status(400)
-          .json({ error: "todos los campos son obligatorios" });
+          .json({ error: 'todos los campos son obligatorios' });
       }
       const query =
         'INSERT INTO product (title, description, price, category_id) VALUES (?, ?, ?, ?)';
-    let result = await executeQuery(query, [title, description, price, category_id]);
-      res.status(200).json({product_id: result.insertId});
+      let result = await executeQuery(query, [
+        title,
+        description,
+        price,
+        category_id,
+      ]);
+      res.status(200).json({ product_id: result.insertId });
     } catch (error) {
       console.log(error);
-      res
-        .status(500)
-        .json({ error: 'error al crear el producto'});
+      res.status(500).json({ error: 'error al crear el producto' });
     }
   };
 
@@ -36,9 +39,9 @@ class ProductsController {
         id,
       ]);
       if (result.affectedRows === 0) {
-        return res.status(404).json({ error: "Producto no encontrado" });
+        return res.status(404).json({ error: 'Producto no encontrado' });
       }
-      res.status(200).json("/admin/productos");
+      res.status(200).json('/admin/productos');
     } catch (error) {
       console.log(error);
       res.status(500).json({ error: 'Error al actualizar el producto' });
@@ -50,10 +53,10 @@ class ProductsController {
       const { id } = req.params;
 
       const query = 'UPDATE product SET is_deleted = 1 WHERE product_id = ?';
-      const [result] = await executeQuery(query, [id]);
+      const result = await executeQuery(query, [id]);
 
       if (result.affectedRows === 0) {
-        return res.status(404).json({ error: "Producto no encontrado" });
+        return res.status(404).json({ error: 'Producto no encontrado' });
       }
       res.status(200).json({ message: 'Producto desactivado correctamente' });
     } catch (error) {
@@ -65,12 +68,12 @@ class ProductsController {
   getProducts = async (req, res) => {
     try {
       const products = await executeQuery(
-        "SELECT * FROM product WHERE is_deleted = 0"
+        'SELECT * FROM product WHERE is_deleted = 0'
       );
       res.status(200).json(products);
     } catch (error) {
-      console.log("Error en getProducts:", error);
-      res.status(500).json({ error: "error al obtener los productos" });
+      console.log('Error en getProducts:', error);
+      res.status(500).json({ error: 'error al obtener los productos' });
     }
   };
 
@@ -78,17 +81,17 @@ class ProductsController {
     try {
       const { id } = req.params;
       const sql =
-        "SELECT * FROM product WHERE product_id = ? AND is_deleted = 0";
+        'SELECT * FROM product WHERE product_id = ? AND is_deleted = 0';
       const result = await executeQuery(sql, [id]);
 
       if (result.length === 0) {
-        return res.status(404).json({ error: "Producto no encontrado" });
+        return res.status(404).json({ error: 'Producto no encontrado' });
       }
 
       res.status(200).json(result[0]);
     } catch (error) {
-      console.log("Error al obtener el producto:", error);
-      res.status(500).json({ error: "Error interno del servidor" });
+      console.log('Error al obtener el producto:', error);
+      res.status(500).json({ error: 'Error interno del servidor' });
     }
   };
 }
