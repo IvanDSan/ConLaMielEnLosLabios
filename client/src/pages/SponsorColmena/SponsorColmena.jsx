42ec5761b0cd9a814930c2cdc2a26d19e0166e51
Pedro <pedrodecnop@hotmail.com>
2025-03-19 11:22:01 +0100

    fix

--- client/src/pages/SponsorColmena/SponsorColmena.jsx
+++ client/src/pages/SponsorColmena/SponsorColmena.jsx
@@ -1,30 +1,44 @@
-import React from 'react';
-import './Styles.css';
+import React from "react";
+import "./Styles.css";
 
 export const SponsorColmena = () => {
   const subscriptionTiers = [
     {
-      title: 'Single',
-      description: 'Patrocinio anual individual que incluye acceso a contenido exclusivo y descuentos en productos.',
-      price: '70.00€',
+      title: "Single",
+      description:
+        "Patrocinio anual individual que incluye acceso a contenido exclusivo y descuentos en productos.",
+      price: "70.00€",
     },
     {
-      title: 'Pro',
-      description: 'Patrocinio premium con ventajas exclusivas como visitas a colmenares y productos personalizados.',
-      price: '110.00€',
+      title: "Pro",
+      description:
+        "Patrocinio premium con ventajas exclusivas como visitas a colmenares y productos personalizados.",
+      price: "110.00€",
     },
   ];
   const galleryImages = [
-    '/images/colmenas/caja1.png',
-    '/images/colmenas/caja2.png',
-    '/images/colmenas/caja3.png',
-    '/images/colmenas/caja4.png',
+    "/images/colmenas/caja1.png",
+    "/images/colmenas/caja2.png",
+    "/images/colmenas/caja3.png",
+    "/images/colmenas/caja4.png",
   ];
 
   const testimonials = [
-    { comment: '¡Gran iniciativa para salvar abejas!', name: 'María G.', rating: '★★★★★' },
-    { comment: 'La miel es deliciosa y apoyo una buena causa.', name: 'Juan P.', rating: '★★★★★' },
-    { comment: 'Excelente servicio y compromiso.', name: 'Ana R.', rating: '★★★★★' },
+    {
+      comment: "¡Gran iniciativa para salvar abejas!",
+      name: "María G.",
+      rating: "★★★★★",
+    },
+    {
+      comment: "La miel es deliciosa y apoyo una buena causa.",
+      name: "Juan P.",
+      rating: "★★★★★",
+    },
+    {
+      comment: "Excelente servicio y compromiso.",
+      name: "Ana R.",
+      rating: "★★★★★",
+    },
   ];
 
   return (
@@ -32,7 +46,10 @@ export const SponsorColmena = () => {
       <div className="header">
         <h1>¡HAZ LA DIFERENCIA!</h1>
         <h1>Apadrina una colmena y salva abejas</h1>
-        <p>Disfruta de miel exclusiva, ayuda a la biodiversidad y vive una experiencia única en el campo</p>
+        <p>
+          Disfruta de miel exclusiva, ayuda a la biodiversidad y vive una
+          experiencia única en el campo
+        </p>
       </div>
       <div className="subscription-tiers">
         {subscriptionTiers.map((tier, index) => (
